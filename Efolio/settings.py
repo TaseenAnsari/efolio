38b5d19e88126f27ac266ef220ac6f1bfedaf66e
TaseenAnsari <63318687+TaseenAnsari@users.noreply.github.com>
2021-08-02 04:09:47 +0530

    Update settings.py

--- Efolio/settings.py
+++ Efolio/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from .stroages import MediaStorages
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
